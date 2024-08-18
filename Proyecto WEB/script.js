aa428fa9cc974bd688d92ffa59020d19eed51fca
Chesantz <smunozriv@gmail.com>
2024-08-18 15:09:18 -0600

    Add files via upload

---
+++ Proyecto WEB/script.js
@@ -0,0 +1,174 @@
+function setActive(link) {
+    // Elimina la clase 'active' de todos los enlaces
+    const links = document.querySelectorAll('.nav-links a');
+    links.forEach((item) => {
+        item.classList.remove('active');
+    });
+
+    // Agrega la clase 'active' al enlace que se clicó
+    link.classList.add('active');
+}
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    clearInterval(intervalId);
+    slideIndex++;
+    showSlide(slideIndex);
+    intervalId = setInterval(nextSlide, 5000);
+}
+
+// Access the Images
+let slideImages = document.querySelectorAll('.wrapper img');
+// Access the next and prev buttons
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+// Access the indicators
+let dots = document.querySelectorAll('.dot');
+
+var counter = 0;
+
+// Code for next button
+next.addEventListener('click', slideNext);
+function slideNext(){
+slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+if(counter >= slideImages.length-1){
+    counter = 0;
+}
+else{
+    counter++;
+}
+slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
+indicators();
+}
+
+// Code for prev button
+prev.addEventListener('click', slidePrev);
+function slidePrev(){
+slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
+if(counter == 0){
+    counter = slideImages.length-1;
+}
+else{
+    counter--;
+}
+slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
+indicators();
+}
+
+// Auto slideing
+function autoSliding(){
+    deletInterval = setInterval(timer, 3000);
+    function timer(){
+        slideNext();
+        indicators();
+    }
+}
+autoSliding();
+
+// Stop auto sliding when mouse is over
+const container = document.querySelector('.container');
+container.addEventListener('mouseover', function(){
+    clearInterval(deletInterval);
+});
+
+// Resume sliding when mouse is out
+container.addEventListener('mouseout', autoSliding);
+
+// Add and remove active class from the indicators
+function indicators(){
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    dots[counter].className += ' active';
+}
+
+// Add click event to the indicator
+function switchImage(currentImage){
+    currentImage.classList.add('active');
+    var imageId = currentImage.getAttribute('attr');
+    if(imageId > counter){
+    slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+    counter = imageId;
+    slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
+    }
+    else if(imageId == counter){
+        return;
+    }
+    else{
+    slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
+    counter = imageId;
+    slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';	
+    }
+    indicators();
+}
+
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
+    console.log(link);
+  }
+})
+
+$(document).ready(function(){
+
+	$('.btn-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.btn-arriba').slideDown(300);
+		} else {
+			$('.btn-arriba').slideUp(300);
+		}
+	});
+
+});
+
+$(document).ready(function(){
+    $('.cn').click(function(){
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
+    })
+
+    $(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.btn-arriba').slideDown(300);
+		} else {
+			$('.btn-arriba').slideUp(300);
+		}
+	});
+
+})
